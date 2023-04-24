6dcef76445654c34a808e2f2b48e5c4845751925
Bal Elsada Hasun <fouadhason@gmail.com>
2023-04-24 11:26:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+let main = document.getElementById('main').remove()
+
+let newHeader = document.createElement('h1')
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = 'Bal is the champion'
